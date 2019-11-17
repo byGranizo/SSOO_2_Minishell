15d950bfaaa62c6440424e408f3ecf75398d0cb3
Miguel Jimenez <migueljg1997@gmail.com>
2019-11-17 14:12:05 +0100

    first line, can execute

--- myshell.c
+++ myshell.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
-
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "parser.h"
+
+int instruccionNormal(tline * line){
+	pid_t pid;
+	pid = fork();
+	if (pid < 0){
+		//fallo
+	} else if(pid == 0) {
+		//hijo
+		execvp(line->commands[0].argv[0], line->commands[0].argv);
+		exit(0);
+	} else {
+		//padre
+		waitpid(pid, NULL, 0);
+	}
+}
 
 int main(int argc){
     char buf[1024];
 	tline * line;
+	pid_t pid;
     int i;
+	int result;
 
     if(argc != 1){
         return 1;
@@ -15,9 +35,22 @@ int main(int argc){
 	while (fgets(buf, 1024, stdin)) {
 		
 		line = tokenize(buf);
-		for (i=0; i<line->commands[0].argc; i++) {
-				printf("  argumento %d: %s\n", i, line->commands[0].argv[i]);
-			}
+		if (line==NULL) {
+			continue;
+		}
+		if (line->redirect_input != NULL) {
+			
+		}
+		if (line->redirect_output != NULL) {
+			
+		}
+		if (line->redirect_error != NULL) {
+			
+		}
+		if (line->background) {
+			
+		}
+		
 		printf("msh> ");	
 	}
 }
